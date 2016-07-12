aaca2dee8c9cd0c994b927bb6ae0112cc1bcf202
Vrexas <giuseppe.ristaino@gmail.com>
2016-07-12 13:32:56 +0200

    Commit Iniziale

---
+++ Giuliya/app/src/main/java/com/software/ing/giuliya/MainActivity.java
@@ -0,0 +1,112 @@
+package com.software.ing.giuliya;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etResponse;
+    TextView tvIsConnected;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get reference to the views
+        etResponse = (EditText) findViewById(R.id.etResponse);
+        tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
+
+        // check if you are connected or not
+        if(isConnected()){
+            tvIsConnected.setBackgroundColor(0xFF00CC00);
+            tvIsConnected.setText("You are conncted");
+        }
+        else{
+            tvIsConnected.setText("You are NOT conncted");
+        }
+
+        // call AsynTask to perform network operation on separate thread
+        new HttpAsyncTask().execute("http://hmkcode.com/examples/index.php");
+    }
+
+    // check network connection
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GetOCR();
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            etResponse.setText(result);
+        }
+    }
+
+    public static String GetOCR()
+    {
+        HttpClient httpclient = new DefaultHttpClient();
+        String result = "";
+
+        try
+        {
+            Uri.Builder builder = new Uri.Builder();
+            builder.scheme("https")
+                    .authority("api.projectoxford.ai")
+                    .appendPath("vision")
+                    .appendPath("v1.0")
+                    .appendPath("ocr")
+                    .appendQueryParameter("language", "it")
+                    .appendQueryParameter("detectOrientation", "true");
+
+            HttpPost request = new HttpPost(builder.build().toString());
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", "56324626a88748adab5e777feb795aae");
+
+
+            // Request body
+            StringEntity reqEntity = new StringEntity("{\"url\":\"http://scrineum.unipv.it/rivista/nicolaj/scontrino.jpg\"}");
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                result = EntityUtils.toString(entity);
+            }
+
+
+        }
+        catch (Exception e)
+        {
+            result = e.getMessage();
+        }
+        return result;
+    }
+}
